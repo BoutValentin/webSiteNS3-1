ad702bdf826254259c4b817f964a3b51fa7aed93
Valentin Bout <bout.valentin@gmail.com>
2022-05-11 19:04:47 +0200

    fix: bad syntax deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.assets.compile = true config.assets.digest = true
+  config.assets.compile = true 
+  config.assets.digest = true
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
